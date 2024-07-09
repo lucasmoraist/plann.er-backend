505e8277aef3cc35755e7db581c7593138e9070a
lucasmoraist <luksmnt1101@gmail.com>
2024-07-09 14:15:07 -0300

    feat: Add participation service for registering participants and sending confirmation emails

---
+++ src/main/java/com/lucasmoraist/planner/participant/ParticipationService.java
@@ -0,0 +1,19 @@
+package com.lucasmoraist.planner.participant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipationService {
+
+    public void registerParticipantToEvent(List<String> participantsToInvite, UUID tripId){
+
+    }
+
+    public void triggerConfirmationEmailToParticipant(UUID tripId){
+
+    }
+
+}
