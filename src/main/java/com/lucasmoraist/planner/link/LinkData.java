c617ed794f31a6ea8b3e33b8092cac6b2547fbdb
lucasmoraist <luksmnt1101@gmail.com>
2024-07-10 11:27:46 -0300

    feat: Add LinkData, LinkRequestPayload, and LinkResponse classes for managing link information

---
+++ src/main/java/com/lucasmoraist/planner/link/LinkData.java
@@ -0,0 +1,6 @@
+package com.lucasmoraist.planner.link;
+
+import java.util.UUID;
+
+public record LinkData(UUID id, String title, String url) {
+}
---
+++ src/main/java/com/lucasmoraist/planner/link/LinkResponse.java
@@ -0,0 +1,6 @@
+package com.lucasmoraist.planner.link;
+
+import java.util.UUID;
+
+public record LinkResponse(UUID linkId) {
+}
