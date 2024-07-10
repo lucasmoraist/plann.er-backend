3466b7a836f1b1d793312c3f79141238d66a9be2
lucasmoraist <luksmnt1101@gmail.com>
2024-07-10 10:19:14 -0300

    refactor: Add Participant entity and ParticipantRepository interface for managing participant information

---
+++ src/main/java/com/lucasmoraist/planner/participant/Participant.java
@@ -0,0 +1,41 @@
+package com.lucasmoraist.planner.participant;
+
+import com.lucasmoraist.planner.trips.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Table(name = "participants")
+@Entity(name = "participants")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participant {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private boolean isConfirmed;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+    public Participant(String email, Trip trip){
+        this.email = email;
+        this.trip = trip;
+        this.isConfirmed = false;
+        this.name = "";
+    }
+
+}
