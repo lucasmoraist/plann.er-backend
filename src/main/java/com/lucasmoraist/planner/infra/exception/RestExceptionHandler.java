7b3fd42d5c65fd3953af3c9f9c591ee494e98566
lucasmoraist <luksmnt1101@gmail.com>
2024-07-10 13:11:41 -0300

    refactor: Update RestExceptionHandler to handle additional exceptions

--- src/main/java/com/lucasmoraist/planner/infra/exception/RestExceptionHandler.java
+++ src/main/java/com/lucasmoraist/planner/infra/exception/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.lucasmoraist.planner.infra.exception;
 
+import com.lucasmoraist.planner.exception.DatesInconsistency;
 import com.lucasmoraist.planner.exception.ExceptionDTO;
 import com.lucasmoraist.planner.exception.ResourceNotFound;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -9,12 +10,34 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.DateTimeException;
+
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFound.class)
     protected ResponseEntity<ExceptionDTO> handleResourceNotFound(ResourceNotFound e) {
         return ResponseEntity.badRequest().body(new ExceptionDTO(e.getMessage(), HttpStatus.NOT_FOUND));
+    }
+
+    @ExceptionHandler(DatesInconsistency.class)
+    protected ResponseEntity<ExceptionDTO> handleDatesInconsistency(DatesInconsistency e) {
+        return ResponseEntity.badRequest().body(new ExceptionDTO(e.getMessage(), HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    protected ResponseEntity<ExceptionDTO> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().body(new ExceptionDTO(e.getMessage(), HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(DateTimeException.class)
+    protected ResponseEntity<ExceptionDTO> handleDateTimeException(DateTimeException e) {
+        return ResponseEntity.badRequest().body(new ExceptionDTO("DateTime written incorrectly", HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    protected ResponseEntity<ExceptionDTO> handleNullPointerException(NullPointerException e) {
+        return ResponseEntity.badRequest().body(new ExceptionDTO("Mandatory values not filled", HttpStatus.BAD_REQUEST));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
@@ -26,10 +49,4 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<ExceptionDTO> handleException(Exception e) {
         return ResponseEntity.badRequest().body(new ExceptionDTO(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
     }
-
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    protected ResponseEntity<ExceptionDTO> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
-        return ResponseEntity.badRequest().body(new ExceptionDTO(e.getMessage(), HttpStatus.BAD_REQUEST));
-    }
-
 }
