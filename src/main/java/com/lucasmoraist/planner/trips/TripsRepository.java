b79d33a105ee8454a2790fbea0541c659460dc56
lucasmoraist <luksmnt1101@gmail.com>
2024-07-10 10:20:17 -0300

    refactor: Remove unused Trips-related code

--- src/main/java/com/lucasmoraist/planner/trips/TripsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lucasmoraist.planner.trips;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
-
-@Repository
-public interface TripsRepository extends JpaRepository<Trips, UUID> {}
