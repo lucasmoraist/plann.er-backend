b719ee83e85f57f2ccf2a1903614984831e14e57
lucasmoraist <luksmnt1101@gmail.com>
2024-07-10 10:20:14 -0300

    feat: Add TripRepository interface for managing trip information

---
+++ src/main/java/com/lucasmoraist/planner/trips/TripRepository.java
@@ -0,0 +1,9 @@
+package com.lucasmoraist.planner.trips;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TripRepository extends JpaRepository<Trip, UUID> {}
