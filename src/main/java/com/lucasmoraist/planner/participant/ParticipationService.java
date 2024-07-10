48be9c5e7b5f2be88f4a74cd593c22fb31a493eb
lucasmoraist <luksmnt1101@gmail.com>
2024-07-10 10:19:58 -0300

    refactor: Update ParticipationService to register multiple participants and send confirmation emails

--- src/main/java/com/lucasmoraist/planner/participant/ParticipationService.java
+++ src/main/java/com/lucasmoraist/planner/participant/ParticipationService.java
@@ -1,5 +1,7 @@
 package com.lucasmoraist.planner.participant;
 
+import com.lucasmoraist.planner.trips.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,12 +10,40 @@ import java.util.UUID;
 @Service
 public class ParticipationService {
 
-    public void registerParticipantToEvent(List<String> participantsToInvite, UUID tripId){
+    @Autowired
+    private ParticipantRepository participantRepository;
+
+    public void registerParticipantsToEvent(List<String> participantsToInvite, Trip trip){
+        List<Participant> participants = participantsToInvite
+                .stream()
+                .map((email -> new Participant(email, trip)))
+                .toList();
+
+        this.participantRepository.saveAll(participants);
+
+        System.out.println(participants.get(0).getId());
 
     }
 
-    public void triggerConfirmationEmailToParticipant(UUID tripId){
+    public ParticipationCreateResponse registerParticipantToEvent(String email, Trip trip){
+        Participant participant = new Participant(email, trip);
+        this.participantRepository.save(participant);
+        return new ParticipationCreateResponse(participant.getId());
+    }
 
+    public void triggerConfirmationEmailToParticipants(UUID tripId){
+
+    }
+
+    public void triggerConfirmationEmailToParticipant(String email){
+
+    }
+
+    public List<ParticipantsData> getAllParticipants(UUID tripId){
+        return this.participantRepository.findByTripId(tripId)
+                .stream()
+                .map(participant -> new ParticipantsData(participant.getId(), participant.getName(), participant.getEmail(), participant.isConfirmed()))
+                .toList();
     }
 
 }
