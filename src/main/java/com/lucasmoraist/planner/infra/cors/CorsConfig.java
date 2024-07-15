a15d8ccbbbaa460ea97cafb4d4e0804443dce2c3
Lucas de Morais <112484844+lucasmoraist@users.noreply.github.com>
2024-07-15 13:18:47 -0300

    Update CorsConfig.java

--- src/main/java/com/lucasmoraist/planner/infra/cors/CorsConfig.java
+++ src/main/java/com/lucasmoraist/planner/infra/cors/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://lucasmoraist.github.io/plann.er-frontend")
+                        .allowedOrigins("http://localhost:3000", "https://lucasmoraist.github.io/plann.er-frontend/")
                         .allowedMethods("GET", "POST", "DELETE", "PUT");
             }};
     }
